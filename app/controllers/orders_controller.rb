311a4e73e701532ba6ede408ead3318af9669295
Joathan Souza <joathanf@gmail.com>
2024-06-08 04:41:22 -0300

    feat: add OrdersController with CRUD actions for orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class OrdersController < ApplicationController
+  def index
+    orders = Order.all
+    render json: orders
+  end
+
+  def show
+    order = Order.find(params[:id])
+    render json: order
+  end
+
+  def create
+    order = Order.new(order_params)
+    if order.save
+      render json: order, status: :created
+    else
+      render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    order = Order.find(params[:id])
+    if order.update(order_params)
+      render json: order, status: :ok
+    else
+      render json: { errors: order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    order = Order.find(params[:id])
+    order.destroy
+    head :no_content
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :product_id, :quantity)
+  end
+end
